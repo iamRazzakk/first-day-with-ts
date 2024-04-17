555928cfbe53b894074edacda2c08b00654a4e92
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-04-17 09:03:10 +0600

    work on array and object destructuring and splate operator

---
+++ src/ArrayAndObject.ts
@@ -0,0 +1,9 @@
+// array and object Destructuring
+const bestFriend = {
+    fullName: "Mahbubul Ahsan Nayem",
+    age: 24
+}
+// const { fullName } = bestFriend
+// console.log(fullName)
+const { fullName: name } = bestFriend
+console.log(name)
--- src/Function.ts
+++ src/Function.ts
@@ -2,7 +2,7 @@
 function sumNumber(num1: number, num2: number) {
     return num1 + num2
 }
-console.log(sumNumber(10, 20))
+// console.log(sumNumber(10, 20))
 
 // Arrow Function
 const sumNumber2 = (num1: number, num2: number) => {
@@ -13,7 +13,7 @@ sumNumber2(20, 30)
 // Callback Function
 const arr = [1, 5, 6, 78, 9, 5]
 const newArray = arr.map((newArr: number) => newArr * newArr)
-console.log(newArray)
+// console.log(newArray)
 
 // Object in normal function
 const person: {
@@ -24,6 +24,24 @@ const person: {
     balance: 20,
     addBalance: (money: number = 20) => {
         person.balance += money;
-console.log(`My Total Balance is ${person.balance}`);
+// console.log(`My Total Balance is ${person.balance}`);
     }
 }
+// Spread Operator
+const myFriend = ['Rakib', "Shakil", "kawsar"]
+const newFriend = ["Nayem", "akib", "Shakib", "Tanvir"]
+myFriend.push(...newFriend)
+// console.log(myFriend)
+
+
+//Rest Parameters
+const greatFriend: (
+    friend1: string,
+    friend2: string,
+    friend3: string
+) => void;
+const greatFriend: GreatFriendType = function (friend1, friend2, friend3) {
+    console.log(`hi ${friend1}\nhi ${friend2}\nhi ${friend3}`);
+};
+
+greatFriend("Rakib", "Shakib", "Shakil");
