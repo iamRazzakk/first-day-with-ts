1d4bdc673d64dc304be58e557e5de13276905a9f
iamRazzakk <rakibt23p@gmail.com>
2024-05-03 12:29:44 -0400

    git commit

--- src/TernaryOptionalChaining&NullishCoalescingOperator.ts
+++ src/TernaryOptionalChaining&NullishCoalescingOperator.ts
@@ -1,22 +1,22 @@
-// const myAge: number = 10
-// if (myAge > 9) {
-//     console.log("Hello")
-// } else {
-//     console.log("Hi")
-// }
+const myAge: number = 10
+if (myAge > 9) {
+    console.log("Hello")
+} else {
+    console.log("Hi")
+}
 
-// myAge > 6 ? console.log("Hello Brother") : console.log("Hi brother")
+myAge > 6 ? console.log("Hello Brother") : console.log("Hi brother")
 
 // Nullish Operator
-// const isAdult = null;
-// const isAdmin = undefined;
-// const isRakib = ""
-// const result = isRakib ?? "Rakib"
-// const result1 = isAdult ?? "Hello Brother go to mosjid"
-// const result2 = isAdmin ?? "Hello Brother go to mosjid"
-// console.log(result)
-// console.log(result1)
-// console.log(result2)
+const isAdult = null;
+const isAdmin = undefined;
+const isRakib = ""
+const result = isRakib ?? "Rakib"
+const result1 = isAdult ?? "Hello Brother go to mosjid"
+const result2 = isAdmin ?? "Hello Brother go to mosjid"
+console.log(result)
+console.log(result1)
+console.log(result2)
 type USER = {
     name: string,
     age: number,
