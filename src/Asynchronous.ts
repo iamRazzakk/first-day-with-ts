bfdcc83684801c95c002821bc3383e219241f911
iamRazzakk <rakibt23p@gmail.com>
2024-05-10 11:06:09 -0400

    work on asynchronous function is typescript

--- src/Asynchronous.ts
+++ src/Asynchronous.ts
@@ -1,4 +1,25 @@
 {
+
+    type dataType = {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean,
+    }
+    const fetchData = async (): Promise<dataType> => {
+        const respone = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const data = await respone.json()
+        return data
+    }
+    fetchData()
+        .then(res => {
+            console.log(res)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+
     const createPromise = () => {
         return new Promise((res, rej) => {
             const data: string = "Something"
@@ -9,4 +30,10 @@
             }
         })
     }
+
+    const showData = async () => {
+        const data = await createPromise()
+        console.log(data)
+    }
+    showData()
 }
