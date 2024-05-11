9d88d2793b1b2c7b3a61c2f3d5405c0e296d81e2
iamRazzakk <rakibt23p@gmail.com>
2024-05-10 21:53:14 -0400

    Learn Inheritance in oop

--- src/OOP/Class.ts
+++ src/OOP/Class.ts
@@ -1,28 +1,28 @@
 {
 
     // Understanding Classes:
-    // class Animal {
-    //     name: string;
-    //     specific: string;
-    //     sound: string
-    //     // Create an instance of animal
+    class Animal {
+        name: string;
+        specific: string;
+        sound: string
+        // Create an instance of animal
 
-    //     constructor(name: string, specific: string, sound: string) {
-    //         this.name = name;
-    //         this.specific = specific
-    //         this.sound = sound;
-    //     }
+        constructor(name: string, specific: string, sound: string) {
+            this.name = name;
+            this.specific = specific
+            this.sound = sound;
+        }
 
-    //     // When you create a function in Class its call method
-    //     makeSound() {
-    //         console.log(`This ${this.name} says ${this.sound}`)
-    //     }
-    //     // console the animal name and his sound
-    // }
-    // const cat = new Animal("Aduri", "cat", "Meaw Meaw")
-    // const Dog = new Animal ("Tiger", "Dog", "Geow Geow")
-    // Dog.makeSound()
-    // Output : This Tiger says Geow Geow
+        // When you create a function in Class its call method
+        makeSound() {
+            console.log(`This ${this.name} says ${this.sound}`)
+        }
+        // console the animal name and his sound
+    }
+    const cat = new Animal("Aduri", "cat", "Meaw Meaw")
+    const Dog = new Animal ("Tiger", "Dog", "Geow Geow")
+    Dog.makeSound()
+    Output : This Tiger says Geow Geow
 
     // Class Inheritance:
 
@@ -46,20 +46,20 @@
 
 
     // Represents an animal with a name (private).
-    // class Animal {
-    //     private name: string; // This is a member variable
+    class Animal {
+        private name: string; // This is a member variable
 
-    //     constructor(name: string) { 
-    //         this.name = name; // This assigns the parameter 'name' to the member variable 'name'
-    //     }
+        constructor(name: string) { 
+            this.name = name; // This assigns the parameter 'name' to the member variable 'name'
+        }
 
-    //     move(distance: number) {
-    //         console.log(`${this.name} moved ${distance} meters.`);
-    //     }
-    // }
+        move(distance: number) {
+            console.log(`${this.name} moved ${distance} meters.`);
+        }
+    }
 
-    // const animal = new Animal("Cat");
-    // console.log(animal.name); 
+    const animal = new Animal("Cat");
+    console.log(animal.name); 
 
 
     // Protected Access Modifier
---
+++ src/OOP/Inheritance.ts
@@ -0,0 +1,43 @@
+{
+
+    class Person {
+        name: string;
+        age: number;
+        adress: string;
+        phone: number;
+        email?: string
+        constructor(name: string, age: number, adress: string, phone: number, email: string) {
+            this.name = name;
+            this.age = age;
+            this.adress = adress;
+            this.phone = phone;
+            this.email = email
+        }
+    }
+
+
+    class Student extends Person {
+        constructor(name: string, age: number, adress: string, phone: number, email: string) {
+            super(name, age, adress, phone, email)
+        }
+
+        getSleep(numberOfHour: number) {
+            console.log(`${this.name} will sleep for ${numberOfHour}`)
+        }
+    }
+    const student1 = new Student("Md Abdur Razzak", 23, "Chittagong", +8801609502136, "mdabdurrazzakrakib290@gmail.com");
+    student1.getSleep(8)
+    class Teacher extends Person {
+        designation: string;
+        constructor(name: string, age: number, adress: string, phone: number, email: string, designation: string) {
+            super(name, age, adress, phone, email)
+            this.designation = designation;
+        }
+        getClass(numberOfHour: number) {
+            console.log(`${this.name} will take class ${numberOfHour}`)
+        }
+    }
+    const Teacher1 = new Teacher("Md Rakib", 30, "Chittagong", +8801878265664, "Mdabdurrazzakrakib290@gmail.com", "seniour teacher")
+    Teacher1.getClass(8)
+
+}
